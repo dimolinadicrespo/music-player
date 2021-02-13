514b2292fde40015c2caf5389fbd577013c97c51
Diego Molina <molinacrespo@gmail.com>
2021-02-13 13:36:15 +0100

    Helper for fotmat time

---
+++ src/helpers.js
@@ -0,0 +1,10 @@
+export const formatTime = (time) => {   
+    if (isNaN(time))
+        return `0:00`;
+    // Minutes and seconds        
+    let mins = Math.round((time / 60), 1);
+    let secs = ~~time % 60;
+    if (secs < 10)
+        secs = `0${secs}`;        
+    return `${mins}:${secs}`;        
+}
