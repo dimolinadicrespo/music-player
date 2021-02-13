300ff3fd6bea37bfd5102371f5088d22d35dc3fc
Diego Molina <molinacrespo@gmail.com>
2021-02-13 13:37:02 +0100

    Library config and styles

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,17 +1,34 @@
-import React, { PureComponent } from 'react';
-import LibraryItem from './LibraryItem';
+import React, { useContext } from "react";
+import LibraryItem from "./LibraryItem";
+import { SongsContext } from "./../hooks/songsContext.js";
 
-const Library = ({songs}) => {
+const Library = () => {
+    const { songs, setSongs } = useContext(SongsContext);
+    const { setCurrentSong, setPlay } = useContext(SongsContext);
+    const { prevSong, setPrevSong } = useContext(SongsContext);
+    const { open, setOpen } = useContext(SongsContext);
+    const { currentSong } = useContext(SongsContext);
+    
+    const updateCurrentSong = (index) => {    
+        songs[prevSong].active = false;
+        songs[index].active = true;
+        setPrevSong(index);          
+        setCurrentSong(songs[index]);
+        setPlay(false);
+    };
+
     return (
-        <div>
-            {songs.map((song, index) => (                
-                <LibraryItem 
-                    cover={song.cover} 
-                    name={song.name} 
-                    artist={song.artist}></LibraryItem>
-            ))}        
+        <div className={`library ${open ? "open" : ""} `}>
+            <h2>Library</h2>
+            {songs.map((song, index) => (
+                <LibraryItem
+                    key={index}
+                    song={song}
+                    updateCurrentSong={() => updateCurrentSong(index)}
+                ></LibraryItem>
+            ))}
         </div>
     );
-}
- 
+};
+
 export default Library;
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,13 +1,30 @@
-import React, { PureComponent } from "react";
-
+import React, { useContext, useEffect, useRef } from "react";
+import {SongsContext} from './../hooks/songsContext';
+import { faCompactDisc } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Song = () => {
+    
+    const { currentSong : {name, cover, artist, audio, color, active}  } = useContext(SongsContext);
+    const { play, setPlay  } = useContext(SongsContext);
+    const { open, setOpen  } = useContext(SongsContext);
+    const imageRef = useRef(null);
+    
+    useEffect(() => {
+        console.log(imageRef);
+    }, [play])
+
     return (
-        <div className="song-container">
-            <h1>Picture</h1>
-            <h2>Name</h2>
-            <h3>Artist</h3>
-            <h4>Album</h4>
+        <div className="song-container">          
+            <header>
+                <a>Smusicfy</a>
+                <button className="btn" onClick={() => setOpen(!open)}>
+                    <FontAwesomeIcon className="library-btn" icon={faCompactDisc} color={'#4e4c4c'}/> Library   
+                </button>
+            </header>
+            <img ref={imageRef} src={cover} alt={name} className={play ? 'cover-play' : ''}/>
+            <h1>{artist}</h1>
+            <h2>{name}</h2>            
         </div>
     );
 };
