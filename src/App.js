c0afbd0fd5d6027d26aeac7dc513556e21cc77d7
Diego Molina <molinacrespo@gmail.com>
2021-02-13 13:36:01 +0100

    Refactor variables to contexapi

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./styles/index.scss";
 import Song from "./components/Song";
 import Player from "./components/Player";
 import Library from './components/Library';
-import chillHop from './data.js';
-import { useState } from "react";
+import SongsProvider from './hooks/songsContext.js'
+import Switcher from './components/Switcher';
 
-function App() {
-    const [songs, setsongs] = useState(chillHop)
-    const [currentSong, setCurrentSong] = useState(null)
+function App() {   
+    
     return (
         <div className="App">
-            <Song></Song>
-            <Player></Player>
-            <Library songs={songs}></Library>
+            <SongsProvider>
+                <Switcher></Switcher>
+                <Song></Song>
+                <Player></Player>
+                <Library></Library>
+            </SongsProvider>
         </div>
     );
 }
